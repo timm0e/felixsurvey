06d317cbc691aac1fb79c580e5baec3ef64107c2
timm0e <11885527+timm0e@users.noreply.github.com>
2018-05-01 19:32:10 +0200

    Make airbnb-style conform

--- routes/surveys.js
+++ routes/surveys.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('surveys');
+router.get('/', (req, res) => {
+    res.render('surveys');
+});
+
+router.get('/:id', (req, res) => {
+    res.render('surveys/answer');
 });
 
 module.exports = router;
